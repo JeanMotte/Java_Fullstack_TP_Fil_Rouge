7afa4281463f081be3412bc2023d0756ae825bec
Jean Motte <jijimotte@gmail.com>
2025-09-25 16:45:56 +0200

    ADD: display list of strings on the run

--- src/main/java/tp_group/tp_fil_rouge/TpFilRougeApplication.java
+++ src/main/java/tp_group/tp_fil_rouge/TpFilRougeApplication.java
@@ -2,12 +2,22 @@ package tp_group.tp_fil_rouge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class TpFilRougeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TpFilRougeApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public List<String> getStrings() {
+        return List.of("Hello", "LSI", "T2/2025-26");
     }
 
 }
