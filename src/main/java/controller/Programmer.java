42983bfe5eb32a851771d45b72cb9d438cd69f98
Jean Motte <jijimotte@gmail.com>
2025-09-25 17:04:31 +0200

    UPDATE: programmer class

--- src/main/java/controller/Programmer.java
+++ src/main/java/controller/Programmer.java
@@ -1,5 +1,8 @@
 package controller;
 
+import lombok.Data;
+
+@Data
 public class Programmer {
     int id;
     String name;
@@ -7,52 +10,4 @@ public class Programmer {
     String languageSkill;
     String favBook;
     Double wage;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getLanguageSkill() {
-        return languageSkill;
-    }
-
-    public void setLanguageSkill(String languageSkill) {
-        this.languageSkill = languageSkill;
-    }
-
-    public String getFavBook() {
-        return favBook;
-    }
-
-    public void setFavBook(String favBook) {
-        this.favBook = favBook;
-    }
-
-    public Double getWage() {
-        return wage;
-    }
-
-    public void setWage(Double wage) {
-        this.wage = wage;
-    }
 }
