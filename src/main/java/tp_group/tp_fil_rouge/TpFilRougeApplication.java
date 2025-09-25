ebee5574fba449ebd62ba0045bc7dcd683e87981
Jean Motte <jijimotte@gmail.com>
2025-09-25 16:51:05 +0200

    UPDATE: display message inside console

--- src/main/java/tp_group/tp_fil_rouge/TpFilRougeApplication.java
+++ src/main/java/tp_group/tp_fil_rouge/TpFilRougeApplication.java
@@ -2,7 +2,6 @@ package tp_group.tp_fil_rouge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,12 +11,13 @@ import java.util.List;
 public class TpFilRougeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TpFilRougeApplication.class, args);
-    }
 
-    @GetMapping("/")
-    public List<String> getStrings() {
-        return List.of("Hello", "LSI", "T2/2025-26");
-    }
+        List<String> messageList = List.of("Hello", "LSI", "2025-26");
 
+        for(String message : messageList) {
+            System.out.println(message);
+        }
+    }
 }
